9e49258c139b4580d78501028815554bc2e7ea6a
kimhaein0809 <laura0809@naver.com>
2022-07-11 18:19:55 +0900

    fdf

---
+++ 07company/js/script.js
@@ -0,0 +1,13 @@
+
+let view = true;
+$(".toggleMenu").click(function(){
+    if( view == true){
+        $(".topMenu").addClass("active");
+        $(".toggleMenu").addClass("active");
+        view = false;
+    }else{
+        $(".topMenu").removeClass("active");
+        $(".toggleMenu").removeClass("active");
+        view = true;
+    }
+})

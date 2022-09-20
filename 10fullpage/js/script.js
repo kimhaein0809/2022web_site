52c118f34ff3027cc991eddf336ce967b612106a
kimhaein0809 <laura0809@naver.com>
2022-09-20 18:17:53 +0900

    full

--- 10fullpage/js/script.js
+++ 10fullpage/js/script.js
@@ -1,6 +1,5 @@
 $("#fullpage").fullpage({
     anchors:["m1st","m2st","m3st","m4st","m5st"],
     navigation:true,
-    navigationPosition:"fp-left",
-    responsiveWidth:900,
+    navigationPosition:"fp-left"
 })

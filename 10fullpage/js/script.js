2d81a0b5e75ae79578b88581295ce341bb2129db
kimhaein0809 <laura0809@naver.com>
2022-09-22 17:32:39 +0900

    gfg

--- 10fullpage/js/script.js
+++ 10fullpage/js/script.js
@@ -3,3 +3,36 @@ $("#fullpage").fullpage({
     navigation:true,
     navigationPosition:"fp-left"
 })
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    }
+  });
+  var swiper = new Swiper(".mySwiper1", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
